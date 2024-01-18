5a5b20d7eddaac846f32a5394fa5307929dfe37c
GuillermoInfiesta <ginfiestap@gmail.com>
2024-01-18 22:50:44 +0100

    abc

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
 import express from "npm:express@4.18.2"
 import {Request, Response} from "npm:express@4.18.2"
+import mongoose from "npm:mongoose@7.6.3"
+import {load} from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+import {ApolloServer} from "npm:@apollo/server@4.9.5"
+import {startStandaloneServer} from "npm:@apollo/server@4.9.5/standalone"
+
+const env = await load();
+
 const miapp = express();
 
 miapp.use(express.json());
 
-miapp.get("/a", (req: Request, res: Response) => {
-  res.send("Holaaaa")
-})
+miapp
+  .get("/a", (req: Request, res: Response) => {
+    res.send("Holaaaa")
+  })
+  .get("/b", (req: Request, res: Response) => {
+    res.send("Adioooss")
+  })
 
 miapp.listen(3000);
